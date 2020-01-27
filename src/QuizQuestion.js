036519c5d45615b918f5b33897787e3c48abd9b3
devmcb03 <60247127+devmcb03@users.noreply.github.com>
2020-01-27 11:05:23 +0800

    QQ2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react'
 
 class QuizQuestion extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { quiz_position: 1 }
-    }
 
-    render() {
+  render() {
         return (
-        <main>
-        <section>
-          <p>// instruction text goes here</p>
-        </section>
-        <section className="buttons">
-          <ul>
-            //quiz question button logic goes here
-          </ul>
-        </section>
-      </main>
+          <main>
+            <section>
+              <p>// instruction text goes here</p>
+            </section>
+            <section className="buttons">
+              <ul>
+                //quiz question button logic goes here
+              </ul>
+            </section>
+          </main>
         )
     }
 }
+
+export default QuizQuestion
