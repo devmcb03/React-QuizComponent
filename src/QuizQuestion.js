4dbc376ce09cc18c32e8b3edb3a7560ec1ee1703
devmcb03 <60247127+devmcb03@users.noreply.github.com>
2020-01-27 11:20:49 +0800

    fasdfasd

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
             </section>
             <section className="buttons">
               <ul>
-                <li>{this.props.quiz_question.answer_options[0]}</li>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position = 1]}/>
               </ul>
             </section>
           </main>
